52c5052ac9ce7cfe711b46cfa008f24b228d74d4
DaggerES <gavazquez@gmail.com>
2016-03-04 11:20:42 +0100

    Update Program.cs

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,11 +1,8 @@
 using System;
 using System.IO;
-using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
-using ConsoleApplication1;
 
 namespace ConsoleApplication
 {
@@ -13,96 +10,65 @@ namespace ConsoleApplication
     {
         private static readonly CryptoBlock ReceiveBlock = new CryptoBlock();
         private static readonly CryptoBlock SendBlock = new CryptoBlock();
-        private static Socket _socket;
-
-        private static Socket ConnectSocket(string server, int port)
-        {
-            Socket s = null;
-            IPHostEntry hostEntry = null;
-
-            // Get host related information.
-            hostEntry = Dns.GetHostEntry(server);
-
-            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
-            // an exception that occurs when the host IP Address is not compatible with the address family
-            // (typical in the IPv6 case).
-            foreach (IPAddress address in hostEntry.AddressList)
-            {
-                IPEndPoint ipe = new IPEndPoint(address, port);
-                Socket tempSocket =
-                    new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-                tempSocket.Connect(ipe);
-
-                if (tempSocket.Connected)
-                {
-                    s = tempSocket;
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            return s;
-        }
+        private static readonly Socket Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
         
         static void Main(string[] args)
         {
-            var server = "fast2.mycccam24.com";
-            var port = 22000;
-            var username = "h9ttr2";
-            var password = "mycccam24";
-
-            _socket = ConnectSocket(server, port);
+            var server = "my.ccamserver.com";
+            var port = 99999;
+            var username = "user";
+            var password = "passw";
+            
+            Socket.Connect(server, port);
             try
             {
                 var helloBytes = new byte[16];
-                _socket.Receive(helloBytes);
-                var ffff = helloBytes;
+                Socket.Receive(helloBytes);
                 Console.WriteLine("Received hello: " + Encoding.UTF8.GetString(helloBytes));
 
-                CheckConnectionChecksum(helloBytes, 16);
-
-                CryptoBlock.cc_crypt_xor(helloBytes);
-
-                SHA1 sha = new SHA1Managed();
-                var sha1Hash = sha.ComputeHash(helloBytes);
-
-                ReceiveBlock.cc_crypt_init(sha1Hash, 20);
-                ReceiveBlock.cc_decrypt(helloBytes, 16);
-
-                SendBlock.cc_crypt_init(helloBytes, 16);
-                SendBlock.cc_decrypt(sha1Hash, 20);
-
-                SendMsg(20, sha1Hash); //Handshake
-                
-                byte[] userName = new byte[20];
-                Array.Copy(GetBytes(username), userName, GetBytes(username).Length);
-                SendMsg(20, userName);
-                
-                byte[] pwd = new byte[63];
-                Array.Copy(GetBytes(password), userName, GetBytes(password).Length);
-                SendBlock.cc_encrypt(pwd, pwd.Length);
-                
-                SendMsg(5, GetBytes("CCcam"));
-                
-                var count = 0;
-                byte[] receiveBytes = new byte[20];
-                while (count == 0 && _socket.Connected)
+                if (CheckConnectionChecksum(helloBytes, 16))
                 {
-                    count = _socket.Receive(receiveBytes);
-                    Thread.Sleep(500);
-                }
+                    CryptoBlock.cc_crypt_xor(helloBytes);
 
-                ReceiveBlock.cc_decrypt(receiveBytes, 20);
-                Console.WriteLine("Received: " + Encoding.Default.GetString(receiveBytes));
+                    SHA1 sha = new SHA1Managed();
+                    var sha1Hash = sha.ComputeHash(helloBytes);
+
+                    ReceiveBlock.cc_crypt_init(sha1Hash, 20);
+                    ReceiveBlock.cc_decrypt(helloBytes, 16);
+
+                    SendBlock.cc_crypt_init(helloBytes, 16);
+                    SendBlock.cc_decrypt(sha1Hash, 20);
+
+                    SendMsg(20, sha1Hash); //Handshake
+
+                    byte[] userName = new byte[20];
+                    Array.Copy(GetBytes(username), userName, GetBytes(username).Length);
+                    SendMsg(20, userName);
+
+                    byte[] pwd = new byte[63];
+                    Array.Copy(GetBytes(password), userName, GetBytes(password).Length);
+                    SendBlock.cc_encrypt(pwd, pwd.Length);
+
+                    byte[] cCcam = new byte[6];
+                    Array.Copy(GetBytes("CCcam"), cCcam, GetBytes("CCcam").Length);
+                    SendMsg(6, cCcam);
+
+                    byte[] receiveBytes = new byte[20];
+                    Socket.Receive(receiveBytes, 20, SocketFlags.None);
+                    ReceiveBlock.cc_decrypt(receiveBytes, 20);
+
+                    Console.WriteLine(Encoding.Default.GetString(receiveBytes) == "CCcam"
+                        ? "SUCCESS!"
+                        : "NO ACK received!");
+                    //I don't understand why the ACK is not received...
+                }
             }
             catch (SocketException e)
             {
                 Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
             }
-            _socket.Close();
+            Socket.Close();
         }
 
         private static bool CheckConnectionChecksum(byte[] buf, int len)
@@ -135,13 +101,13 @@ namespace ConsoleApplication
 
             try
             {
-                _socket.Send(netbuf);
+                Socket.Send(netbuf);
                 return len;
             }
             catch (IOException e)
             {
                 Console.WriteLine("Connection closed while sending");
-                _socket.Close();
+                Socket.Close();
             }
             return -1;
         }
