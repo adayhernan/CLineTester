b4b8b256af6d414f5e943a51788cab488361981e
DaggerES <gavazquez@gmail.com>
2016-03-11 11:46:37 +0100

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -4,11 +4,22 @@ import java.net.UnknownHostException;
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
     	
-        String host = "ccamline.server.com";
-        int port = 9999;
-        String user = "username";
-        String password = "password";
+        String host = "server.clineserver.com";
+        int port = 99999;
+        String user = "user";
+        String password = "pass";
 
-    	new CCcamConnector(host, port, user, password).run();
+    	boolean valid = false;
+    	
+    	try {
+			valid = new CCcamConnector(host, port, user, password).TestCline();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}    	
+    	
+    	if (valid)
+    		System.out.println("CCLine is valid");
+    	else
+    		System.out.println("CCLine is NOT valid");
     }
 }
